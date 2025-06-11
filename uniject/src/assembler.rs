ff09748157ff33195ffed947dd3008831e20c633
Daniel Oppermann <danopperm@gmail.com>
2025-06-11 15:54:35 +0200

    refactor: use iced for the assembler
    I finally had the time to implement it! :)
    
    The usize -> u64 casting is something i heavilly dislike about the
    current implementation, something i will tackle when i have more time on
    my hands

--- uniject/src/assembler.rs
+++ uniject/src/assembler.rs
@@ -1,103 +1,78 @@
-use std::collections::VecDeque;
+use iced_x86::code_asm::*;
 
 pub struct Assembler {
-    asm: VecDeque<u8>
+    asm: CodeAssembler,
 }
 
 impl Assembler {
     pub fn new() -> Self {
         Assembler {
-            asm: VecDeque::new()
+            asm: CodeAssembler::new(64).unwrap(),
         }
     }
 
     pub fn mov_rax(&mut self, arg: usize) {
-        self.asm.extend([0x48, 0xB8]);
-        self.asm.extend(arg.to_le_bytes());
+        self.asm.mov(rax, arg as u64).unwrap();
     }
 
     pub fn mov_rcx(&mut self, arg: usize) {
-        self.asm.extend([0x48, 0xB9]);
-        self.asm.extend(arg.to_le_bytes());
+        self.asm.mov(rcx, arg as u64).unwrap();
     }
 
     pub fn mov_rdx(&mut self, arg: usize) {
-        self.asm.extend([0x48, 0xBA]);
-        self.asm.extend(arg.to_le_bytes());
+        self.asm.mov(rdx, arg as u64).unwrap();
     }
 
     pub fn mov_r8(&mut self, arg: usize) {
-        self.asm.extend([0x49, 0xB8]);
-        self.asm.extend(arg.to_le_bytes());
+        self.asm.mov(r8, arg as u64).unwrap();
     }
 
     pub fn mov_r9(&mut self, arg: usize) {
-        self.asm.extend([0x49, 0xB9]);
-        self.asm.extend(arg.to_le_bytes());
+        self.asm.mov(r9, arg as u64).unwrap();
     }
 
     pub fn sub_rsp(&mut self, arg: u8) {
-        self.asm.extend([0x48, 0x83, 0xEC]);
-        self.asm.push_back(arg);
+        self.asm.sub(rsp, arg as i32).unwrap();
     }
 
     pub fn call_rax(&mut self) {
-        self.asm.extend([0xFF, 0xD0]);
+        self.asm.call(rax).unwrap();
     }
 
     pub fn add_rsp(&mut self, arg: u8) {
-        self.asm.extend([0x48, 0x83, 0xC4]);
-        self.asm.push_back(arg);
+        self.asm.add(rsp, arg as i32).unwrap();
     }
 
     pub fn mov_rax_to(&mut self, dest: usize) {
-        self.asm.extend([0x48, 0xA3]);
-        self.asm.extend(dest.to_le_bytes());
+        self.asm.mov(qword_ptr(dest as u64), rax).unwrap();
     }
 
     pub fn push(&mut self, arg: isize) {
-        if arg >= -128 && arg <= 127 {
-            self.asm.push_back(0x6A);
-            self.asm.push_back(arg as u8);
-        } else {
-            self.asm.push_back(0x68);
-            self.asm.extend((arg as i32).to_le_bytes());
-        }
+        self.asm.push(arg as i32).unwrap();
     }
 
     pub fn mov_eax(&mut self, arg: isize) {
-        self.asm.push_back(0xB8);
-        self.asm.extend((arg as i32).to_le_bytes());
+        self.asm.mov(eax, arg as i32).unwrap();
     }
 
     pub fn call_eax(&mut self) {
-        self.asm.extend([0xFF, 0xD0]);
+        self.asm.call(eax).unwrap();
     }
 
     pub fn add_esp(&mut self, arg: u8) {
-        self.asm.extend([0x83, 0xC4]);
-        self.asm.push_back(arg);
+        self.asm.add(esp, arg as i32).unwrap();
     }
 
     pub fn mov_eax_to(&mut self, dest: usize) {
-        self.asm.push_back(0xA3);
-        self.asm.extend((dest as i32).to_le_bytes());
+        self.asm.mov(dword_ptr(dest as u64), eax).unwrap();
     }
 
     pub fn return_(&mut self) {
-        self.asm.push_back(0xC3);
+        self.asm.ret().unwrap();
     }
 
-    pub fn to_byte_array(&self) -> Vec<u8> {
-        self.asm.iter().cloned().collect()
+    #[allow(clippy::wrong_self_convention)]
+    pub fn to_byte_array(&mut self) -> Vec<u8> {
+        self.asm.assemble(0).unwrap()
     }
-
-    
-    // pub fn get_asm(&self) -> Vec<u8> {
-    //     self.asm.iter().cloned().collect()
-    // }
-
-    // pub fn clear(&mut self) {
-    //     self.asm.clear();
-    // }
 }
