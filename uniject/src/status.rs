f33c5cd8f940d2102d019312774f782bc864534a
Daniel Oppermann <danopperm@gmail.com>
2026-07-31 00:26:08 +0200

    refactor: remove unused case

--- uniject/src/status.rs
+++ uniject/src/status.rs
@@ -12,7 +12,6 @@ impl From<i32> for MonoImageOpenStatus {
             0 => MonoImageOpenStatus::Ok,
             1 => MonoImageOpenStatus::ErrorErrno,
             2 => MonoImageOpenStatus::MissingAssemblyRef,
-            3 => MonoImageOpenStatus::Invalid,
             _ => MonoImageOpenStatus::Invalid,
         }
     }
